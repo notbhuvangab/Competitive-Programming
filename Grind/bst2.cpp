189550f9afa34d8e7853672b5957dcc3b483a0ae
Bhuvan <gbhuvanhyd@gmail.com>
2021-07-11 22:55:43 +0530

    changes

--- Grind/bst2.cpp
+++ Grind/bst2.cpp
@@ -1,5 +1,20 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int m;
+int main(){
+
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    vector<pair<int,int>> vll;
+    
+    for(int i=0;i<5;i++)
+        vll.push_back(make_pair(i,i+1));
+
+    for(int i=0;i<5;i++)
+        cout<<vll[i].first<<" "<<vll[i].second<<endl;
+
+}
